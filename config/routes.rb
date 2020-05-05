fbe0483cc9d618f3aa04a375655dad67ff572172
kazuya sukigara <sukigarakazuya@sukigarakazuyanoMacBook-ea.local>
2020-05-05 21:08:07 +0900

    3

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+  resource  :book_comments, only: [:create]
+  end
+  root 'home#top'
+  get 'home/about'
+
+end
